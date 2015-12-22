421fa9795a05836448ed60c464725170651a2294
JuanHenao <judhenaosa@unal.edu.co>
2015-12-22 18:24:03 -0500

    first version

---
+++ GWAS_MapGene.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[137]:
+
+import urllib2
+
+
+# In[150]:
+
+infile = open("/home/juan/Documentos/GWASCentral_MapGene/test.txt","r")
+outfile = open("output.txt","w")
+
+
+# In[151]:
+
+def MapGene(link):
+    open_link = urllib2.urlopen(link)
+    code_link = open_link.read()
+    
+    init_mark_link = code_link.find("<th>Marker accession ID</th>")
+    white_spaces = code_link[init_mark_link+len("<th>Marker accession ID</th>"):code_link.find("</td>",init_mark_link+len("<th>Marker accession ID</th>"))]
+    ID_SNP = white_spaces[9:-1]
+    
+    init_map_link = code_link.find("<th>Mapped gene</th>")
+    white_spaces_map = code_link[init_map_link+len("<th>Mapped gene</th>"):code_link.find("</td>",init_map_link+len("<th>Marker accession ID</th>"))]
+    map_gene = white_spaces_map[9:]
+    
+    open_link.close()
+    outfile.write(map_gene+"\t"+ID_SNP)
+    
+for i in infile.readlines():
+    pos_web_final = i.find("/",39)
+    MapGene(i[0:pos_web_final])
+
+
+# In[145]:
+
+infile.close()
+outfile.close()
+
+
+# In[ ]:
+
+
+
