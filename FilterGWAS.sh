7ee1daee324b739a82e2728c23a0dcfbeeaf0cdd
JuanHenao <judhenaosa@unal.edu.co>
2015-12-23 01:03:08 -0500

    Filter Complete

--- FilterGWAS.sh
+++ FilterGWAS.sh
@@ -4,8 +4,9 @@ cut -f3 $1 | sort | uniq > .temp
 
 for i in `cat .temp`
 do
-value=`grep -c $i $1`
-if [ $value -ge 2 ];then
-echo $i
-fi
+grep -m 1 $i $1 >> GWAS_Uniq.txt
 done
+
+sort GWAS_Uniq.txt
+rm .temp
+
